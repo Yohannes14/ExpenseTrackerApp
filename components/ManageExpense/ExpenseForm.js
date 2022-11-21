9199156eb82eeec93f61654f2f1f6ea46aa7b3e0
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-21 16:12:33 +0300

    handling user input in a generic ways

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,56 +1,77 @@
-import { StyleSheet, Text, View } from "react-native"
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 const ExpenseForm = () => {
+  const [inputValue, setInputValue] = useState({
+    amount : '',
+    date: '',
+    description: ''
+  });
 
-    function amountChangeHnadler(){}
-    function dateChangeHnadler(){}
+  function inputChangeHnadler(inputIndetifier, enteredInput) {
+    setInputValue((currentValues) => {
+        return {
+            ...currentValues, [inputIndetifier]: enteredInput
+        };
+    });
+  }
 
   return (
-    <View style ={styles.form}>
-        <Text style ={styles.title}>Your Expense</Text>
-    <View style ={styles.inputRow}>
-    <Input style={styles.rowInput}
-    label ="Amount" textInputConfig={{
-        keyboardType: 'decimal-pad',
-        onChangeText: amountChangeHnadler,
-    }} />
-    <Input  style={styles.rowInput}
-    label ="Date" textInputConfig={{
-        placeholder: 'YYYY-MM-DD',
-        maxLength: 10,
-        onChangeText:()=>{},
-    }}/>
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: inputChangeHnadler.bind(this, 'amount'),
+            value: inputValue.amount,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangeHnadler.bind(this, 'date'),
+            value: inputValue.date
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          // autoCapitalize: 'none'
+          // autoCorrent: false
+          onChangeText: inputChangeHnadler.bind(this, 'description'),
+            value: inputValue.description
+        }}
+      />
     </View>
-    <Input
-    label ="Description" textInputConfig={{
-        multiline: true,
-        // autoCapitalize: 'none'
-        // autoCorrent: false
-    }}/>
-    
-    </View>
-  )
-}
+  );
+};
 
 export default ExpenseForm;
 const styles = StyleSheet.create({
-    form: {
-        marginTop: 40,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'white',
-        marginHorizontal: 24,
-        textAlign: 'center',
-    },
-    inputRow: {
-        flexDirection: 'row',
-       justifyContent: 'space-between',
-    },
-    rowInput: {
-        flex: 1,
-    },
-
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginHorizontal: 24,
+    textAlign: "center",
+  },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
 });
